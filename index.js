6fe8bc2c6cb5f665bb498626d2891f3531190d06
Noor Mohammed <mail@noormohammed.tech>
2021-09-14 12:33:07 +0530

    GraphQL Hello World

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const app = require("express")();
 
-const port = process.env.PORT || 9909 
-const userData = require("./MOCK_DATA.json")
+const port = process.env.PORT || 9909;
+const userData = require("./MOCK_DATA.json");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+
+const gqlSchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "helloworld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => {
+          console.log("resolved helloworld");
+          return "Hello World!";
+        },
+      },
+    }),
+  }),
+});
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+    schema: gqlSchema,
+  })
+);
 
 app.listen(port, () => {
-    console.log(`app listening on ${port}`);
-})
+  console.log(`app listening on ${port}`);
+});
