3584ab8cf11a08b79b48a7ce181dc7a88d7b1925
Noor Mohammed <mail@noormohammed.tech>
2021-09-14 13:02:38 +0530

    test query

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ const port = process.env.PORT || 9909;
 const userData = require("./MOCK_DATA.json");
 const expressGraphQL = require("express-graphql").graphqlHTTP;
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+} = require("graphql");
 
-const gqlSchema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "helloworld",
-    fields: () => ({
-      message: {
-        type: GraphQLString,
-        resolve: () => {
-          console.log("resolved helloworld");
-          return "Hello World!";
-        },
+const rootQuery = new GraphQLObjectType({
+  name: "helloworld",
+  fields: () => ({
+    message: {
+      type: GraphQLString,
+      resolve: () => {
+        console.log("resolved helloworld");
+        return "Hello World!";
       },
-    }),
+    },
+    test: {
+      type: GraphQLInt,
+      resolve: () => 4,
+    },
   }),
 });
+const gqlSchema = new GraphQLSchema({ query: rootQuery });
 app.use(
   "/graphql",
   expressGraphQL({
